5d53878a67ac8193805a822bc7df5d5617f0758a
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-06-07 21:39:58 +0200

    Fix highlighting of multiline values

--- highlight.js
+++ highlight.js
@@ -4,4 +4,9 @@ import toml from "highlight.js/lib/languages/ini.js";
 const TOML = "toml";
 hljs.registerLanguage(TOML, toml);
 
-export default (code) => hljs.highlight(TOML, code).value;
+let cache;
+export default (code) => {
+  const result = hljs.highlight(TOML, code, true, cache);
+  cache = result.top;
+  return result.value;
+};
