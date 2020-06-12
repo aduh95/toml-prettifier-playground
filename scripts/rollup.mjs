786d6aa88cbccc006f130a4a0839bdf9e560ebfd
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-06-12 10:47:36 +0200

    Upgrade deps

--- scripts/rollup.mjs
+++ scripts/rollup.mjs
@@ -4,7 +4,7 @@ import cjs2esm from "./rollup-plugin-cjs2esm.mjs";
 import { rollup } from "rollup";
 
 let cache;
-const plugins = [resolve.default({ browser: true }), cjs2esm()];
+const plugins = [resolve({ browser: true }), cjs2esm()];
 
 async function buildWithCache(input) {
   const bundle = await rollup({
