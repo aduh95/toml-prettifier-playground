7ebfbe03cd78f53b01984eaf9192448b605d2c0c
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-08-15 20:51:13 +0200

    Handles non streamed input

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import prettify from "./prettify.js";
 
+const TEMP_SAVED_DATA = "savedData";
+
 const getSelectedLine = (value, selectionStart, selectionEnd) =>
   value.substring(
     Math.max(0, value.lastIndexOf("\n", selectionStart)),
@@ -40,21 +42,71 @@ const getCurrentTOMLExpression = ({ value, selectionStart }) => {
   }
 };
 
+function saveAndReload() {
+  location.reload();
+}
+
 new ReadableStream({
   start(controller) {
     const input = document.createElement("textarea");
     input.autofocus = true;
-    input.addEventListener("input", (event) => controller.enqueue(event));
+    input.addEventListener("input", (event) =>
+      controller.desiredSize
+        ? controller.enqueue(event)
+        : sessionStorage.setItem(TEMP_SAVED_DATA, event.target.value)
+    );
+    input.value = sessionStorage.getItem(TEMP_SAVED_DATA);
     document.body.append(input);
+  },
+  cancel() {
+    const dialog = document.createElement("dialog");
+    const form = document.createElement("form");
+    const button = document.createElement("button");
+
+    form.method = "dialog";
+
+    button.type = "submit";
+    button.textContent = "Prettify";
+
+    form.append(button);
+    dialog.append(form);
+
+    dialog.addEventListener("close", saveAndReload);
+    dialog.open = true;
+
+    document.body.append(dialog);
   },
 })
   .pipeThrough(
     new TransformStream({
-      start() {},
+      start(controller) {
+        const savedData = sessionStorage.getItem(TEMP_SAVED_DATA);
+        if (savedData) {
+          savedData
+            .split("\n")
+            .slice(0, -1)
+            .forEach((line) => controller.enqueue(line));
+          sessionStorage.removeItem(TEMP_SAVED_DATA);
+        }
+      },
       transform(event, controller) {
         const { data, inputType, target: textarea } = event;
         console.log(event);
         if (
+          textarea.selectionStart !== textarea.value.length &&
+          textarea.value.substring(textarea.selectionStart).trim().length === 0
+        ) {
+          textarea.setSelectionRange(
+            textarea.selectionStart,
+            textarea.value.length
+          );
+        } else if (
+          inputType === "insertFromPaste" ||
+          textarea.selectionStart !== textarea.value.length
+        ) {
+          sessionStorage.setItem(TEMP_SAVED_DATA, event.target.value);
+          controller.error(new Error("Input is not streamed."));
+        } else if (
           inputType === "insertLineBreak" ||
           (inputType === "insertText" && data === null)
         ) {
